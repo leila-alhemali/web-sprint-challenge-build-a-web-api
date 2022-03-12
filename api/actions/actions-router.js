210e7075d8c1a0200dd901d3980386c23066e658
Leila <leila.alhemali@gmail.com>
2022-03-11 20:18:00 -0700

    working on actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,21 +3,55 @@ const express = require('express');
 
 const Action = require("./actions-model")
 
-const router = express.Router();
+const router = require("express").Router()
 
 router.get('/', (req, res) => {
-
+    Action.get()
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch( () => {
+            res.status(500).json({ message: "The actions can't be retrieved" })
+        })
 });
 
 router.get('/:id', (req, res) => {
-
+    Action.get(req.params.id)
+    .then((action) => {
+        if (!action) {
+            res.status(404).json({ message: "No action with that id"})
+        } else {
+            res.status(200).json(action)
+        }
+    })
 });
 
 router.post('/', (req, res) => {
-
+    Action.insert(req.body)
+    .then((action) => {
+        console.log(action)
+        if (!req.body.description || !req.body.notes || !req.body.project_id) {
+            res.status(400).json({message: "Description, project ID and notes are required"})
+        } else {
+            res.status(200).json(action)
+        }
+    })
+    .catch(() => {
+        res.status(500).json({message: "Can not post"})
+    })
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedAction = await Action.update(req.params.id, req.body)
+        if (!req.body.description || !req.body.notes || !req.body.project_id) {
+            res.status(400).json({message: "Description, project ID and notes are required"})
+        } else {
+            res.status(200).json(updatedAction)
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Can not update"})
+    }
 
 })
 
